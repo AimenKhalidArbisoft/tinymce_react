9156a19b2ef1fef30a3d9e14920a871fcfb8ff90
aimen.khalid <aimen.khalid@arbisoft.com>
2023-11-21 20:37:11 +0500

    test 2 comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+// test2 comment
 const rootNode = document.getElementById("root");
 ReactDOM.render(<App />, rootNode);
